4deb63b494bf285d101bd8d63635233a8eba51af
Jaredazcarate.work <jaredazcarate.work@gmail.com>
2024-01-06 20:49:43 +0000

    Se incorpora firebase y se crea rama dev para hacer pruebas

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD4aoG39vZF5x5NktdcCYcIGscRX79k0sU",
+  authDomain: "mcdonallds-app.firebaseapp.com",
+  projectId: "mcdonallds-app",
+  storageBucket: "mcdonallds-app.appspot.com",
+  messagingSenderId: "244415454410",
+  appId: "1:244415454410:web:4f8a60cf2d764b9f436833"
+};
+
+// Initialize Firebase
+const app = initializeApp( firebaseConfig );
+
+export const db = getFirestore( app )
